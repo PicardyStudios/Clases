eee3c2bd89c43d1ccc7789af097095003df268b0
Mr Vince <marvince.ar@gmail.com>
2018-12-14 16:00:50 -0300

    14.12.18

--- app/src/main/java/com/picardystudios/clases/MainActivity.java
+++ app/src/main/java/com/picardystudios/clases/MainActivity.java
@@ -3,13 +3,17 @@ package com.picardystudios.clases;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +25,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.camerakit.CameraKitView;
+import com.camerakit.api.camera1.Camera1;
+import com.camerakit.api.camera2.Camera2;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.zxing.client.android.Intents;
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.picardystudios.clases.Util.PermissionUtils;
 
 import java.io.ByteArrayOutputStream;
@@ -35,15 +42,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import static com.picardystudios.clases.Util.AppUtils.enviarPost;
 import static com.picardystudios.clases.Util.AppUtils.showToast;
+import static java.security.AccessController.getContext;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    Uri UriFrenteDNI; File photoFrenteDNI;     Bitmap bitmap;   ImageView mimageView;
+    Uri UriDorsoDNI; File photoDorsoDNI;     Bitmap bitmapD;   ImageView mimageViewD;
+    Uri mImageUriU; File photoU;     Bitmap bitmapU;   ImageView mimageViewU;
+    private static final int CAMERA_REQUEST = 1888;    private static final int CAMERA_DORSODNI = 1889;    private static final int CAMERA_FRENTEDNI = 1890;
+    public final int CUSTOMIZED_REQUEST_CODE = 0x0000ffff;
     private static final String TAG = MainActivity.class.getSimpleName();
-    private CameraKitView cameraKitView;
-    private CameraKitView cameraFrente;
-    private CameraKitView cameraDorso;
-    private CameraKitView cameraTexto;
+    private CameraKitView cameraKitView;    private CameraKitView cameraFrente;
+    private CameraKitView cameraDorso;      private CameraKitView cameraTexto;
     Activity myactivity;
     CameraKitView cameraSelfie;     LinearLayout linear2;   LinearLayout linear3;   LinearLayout linear4; ImageView myImage;
     ImageView imgFrente;    ImageView imgDorso;    ImageView imgTexto;
@@ -51,6 +61,19 @@ public class MainActivity extends AppCompatActivity {
     EditText nombres; EditText apellidos; EditText documento; EditText nacimiento; EditText direccion;
     EditText localidad; Spinner provincia; EditText telfijo; EditText telmovil; EditText email; TextView textoapapel; TextView textoapapelD;
     int TAKE_PHOTO_CODE = 0;    public static int count = 0;    String sFotoDorso = ""; String sFotoFrente= ""; String sFotoTexto= "";
+
+    private File createTemporaryFile(String part, String ext) throws Exception
+    {
+        File tempDir= Environment.getExternalStorageDirectory();
+        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
+        if(!tempDir.exists())
+        {
+            tempDir.mkdirs();
+        }
+        File dis = File.createTempFile(part, ext, tempDir);
+        Log.e("Dis", String.valueOf(dis));
+        return File.createTempFile(part, ext, tempDir);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,9 +91,9 @@ public class MainActivity extends AppCompatActivity {
         linear3= (LinearLayout) findViewById(R.id.linear2);
         linear4= (LinearLayout) findViewById(R.id.linear2);
 
-        cameraKitView = findViewById(R.id.camera);
+        cameraKitView = (CameraKitView) findViewById(R.id.camera);
         cameraFrente =  (CameraKitView) findViewById(R.id.camFrente);
-        cameraDorso =  (CameraKitView) findViewById(R.id.camDorso);
+        cameraDorso =   (CameraKitView) findViewById(R.id.camDorso);
         cameraTexto =  (CameraKitView) findViewById(R.id.camTexto);
         myactivity = this;
 
@@ -81,37 +104,61 @@ public class MainActivity extends AppCompatActivity {
         provincia = findViewById(R.id.provincia);       telfijo = findViewById(R.id.telfijo);
         telmovil = findViewById(R.id.telmovil);         email = findViewById(R.id.email);
         textoapapel = findViewById(R.id.textoapapel);   textoapapelD = findViewById(R.id.textoapapelD);
+
+
+
         EscanearCodigo();
 
 
-        imgFrente.setOnClickListener((View v) -> {
-            imgFrente.setVisibility(View.GONE);
-            cameraFrente.onStart();
+
+        Button bSelfie = (Button) findViewById(R.id.bselfie);
+        bSelfie.setOnClickListener((View v) -> {
+          if(myImage.getVisibility() == View.VISIBLE)  { myImage.setVisibility(View.GONE); cameraKitView.onResume();  Log.e("bSelfie","show camera"); }
+          else if(myImage.getVisibility() == View.GONE)  { captureSave(cameraKitView,0,0);  Log.e("bSelfie","Capture"); }
+          else { Log.e("bSelfie","ni Visible ni Gone"); }
+
         });
 
-        imgDorso.setOnClickListener((View v) -> {
-            imgDorso.setVisibility(View.GONE);
-            cameraDorso.onStart();
+        Button frenteDNI = (Button) findViewById(R.id.frenteDNI);
+        frenteDNI.setOnClickListener((View v) -> {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        try {  photoFrenteDNI = createTemporaryFile("MutualCentral_FrenteDNI", ".jpg");  }
+        catch(Exception e)  { Toast.makeText(getApplicationContext(), "Please check SD card! Image shot is impossible!", Toast.LENGTH_LONG); }
+        UriFrenteDNI = PhotoProvider.getPhotoUri(photoFrenteDNI);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, UriFrenteDNI);
+        // And away we go!
+        startActivityForResult(intent, CAMERA_FRENTEDNI);
+
         });
 
-        imgTexto.setOnClickListener((View v) -> {
-            imgTexto.setVisibility(View.GONE);
-            cameraTexto.onStart();
-        });
 
+
+
+
+
+
+
+
+        Button dorsoDNI = (Button) findViewById(R.id.dorsoDNI);
+        dorsoDNI.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            try {  photoDorsoDNI = createTemporaryFile("MutualCentral_DorsoDNI", ".jpg");  }
+            catch(Exception e)  { Toast.makeText(getApplicationContext(), "Please check SD card! Image shot is impossible!", Toast.LENGTH_LONG); }
+            UriDorsoDNI = PhotoProvider.getPhotoUri(photoDorsoDNI);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, UriDorsoDNI);
+            // And away we go!
+            startActivityForResult(intent, CAMERA_DORSODNI);
+
+        });
 
 
              if (PermissionUtils.isPermissionGranted(this, new String[]{Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},101)) {
                 showToast(this,"Permisos aceptados");
             } else { showToast(this,"Debe aceptar los permisos para continuar..."); }
 
-        myImage.setOnClickListener((View v) -> {
 
-            myImage.setVisibility(View.GONE);
-            //cameraSelfie.onResume(); cameraKitView.setVisibility(View.VISIBLE);
-            linear2.setVisibility(View.GONE);
-
-        });
 
 
         cameraKitView.setGestureListener(new CameraKitView.GestureListener() {
@@ -244,41 +291,47 @@ public class MainActivity extends AppCompatActivity {
 
 
         public void captureSave(CameraKitView cameraKitView, float v, float v1){
+        Log.e("captureSave","Starting...");
 
             cameraKitView.captureImage(new CameraKitView.ImageCallback() {
                 @Override
                 public void onImage(CameraKitView cameraKitView, final byte[] capturedImage) {
+
                     File savedPhoto = new File(Environment.getExternalStorageDirectory(), "photo.jpg");
                     try {
                         FileOutputStream outputStream = new FileOutputStream(savedPhoto.getPath());
                         outputStream.write(capturedImage);
-                        outputStream.close();    Log.e("Captura","OK! "+String.valueOf(savedPhoto));
-                        if(savedPhoto.exists()){
+                        outputStream.close();
+                        Log.e("Captura", "OK! " + String.valueOf(savedPhoto));
+                        if (savedPhoto.exists()) {
                             Bitmap myBitmap = BitmapFactory.decodeFile(savedPhoto.getAbsolutePath());
                             ByteArrayOutputStream baos = new ByteArrayOutputStream();
                             myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, baos); //bm is the bitmap object
                             byte[] b = baos.toByteArray();
-                            String img_str = android.util.Base64.encodeToString(b, 0); Log.e("Base64",img_str);
-                            enviarPost(myactivity,img_str,img_str,img_str);
+                            String img_str = android.util.Base64.encodeToString(b, 0);
+                            Log.e("Base64", img_str);
+                            enviarPost(myactivity, img_str, img_str, img_str);
                             myImage.setImageBitmap(myBitmap);
                             myImage.setVisibility(View.VISIBLE);
-                            //cameraKitView.setVisibility(View.GONE);
+                            cameraKitView.onPause();
                             linear2.setVisibility(View.VISIBLE);
 
                         }
 
                     } catch (java.io.IOException e) {
-                        e.printStackTrace(); Log.e("Camera Exception",e.toString());
+                        e.printStackTrace();
+                        Log.e("Camera Exception", e.toString());
                     }
                 }
-            });
-        }
+                    });
+
+            }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        cameraKitView.onStart();
+
 
     }
 
@@ -286,17 +339,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         cameraKitView.onResume();
+        cameraFrente.onResume();
+        cameraDorso.onResume();
+        cameraTexto.onResume();
+
     }
 
     @Override
     protected void onPause() {
         cameraKitView.onPause();
+        cameraFrente.onPause();
+        cameraDorso.onPause();
+        cameraTexto.onPause();
         super.onPause();
     }
 
     @Override
     protected void onStop() {
         cameraKitView.onStop();
+        cameraFrente.onStop();
+        cameraDorso.onStop();
+        cameraTexto.onStop();
         super.onStop();
     }
 
@@ -304,6 +367,122 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e("requestCode", String.valueOf(requestCode)); Log.e("resultCode", String.valueOf(resultCode));
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+
+
+        if (requestCode == 49374 ) {
+
+            switch (requestCode) {
+                case CUSTOMIZED_REQUEST_CODE: {
+                    Toast.makeText(this, "Datos escaneados, aguarde...", Toast.LENGTH_LONG).show();
+                    // + requestCode
+                    sResult = String.valueOf(requestCode);  ProcesaResultado();
+                    break;
+                }
+                default:
+                    break;
+            }
+
+
+            if(result.getContents() == null) {
+                Log.d("MainActivity", "Verificación cancelada");
+                Toast.makeText(this, "Verificación cancelSda!", Toast.LENGTH_LONG).show();
+                sResult = "Verificación cancelada";   finishActivity(requestCode); //ProcesaResultado();
+            } else {
+                Log.d("MainActivity", "Datos escaneados, aguarde...");
+                Toast.makeText(this, "Datos escaneados, aguarde...", Toast.LENGTH_LONG).show();
+                //+ result.getContents()
+                sResult = result.getContents();  finishActivity(requestCode); ProcesaResultado();
+            }
+
+            /*cameraDorso.onStart();
+            cameraDorso.onPause();
+            cameraTexto.onStart();
+            cameraTexto.onPause();
+            cameraFrente.onStart();
+            cameraFrente.onPause();*/
+            cameraKitView.onStart();
+        }
+
+
+
+        if (requestCode == CAMERA_FRENTEDNI && resultCode == RESULT_OK) {
+            //Bitmap pmphoto = (Bitmap) data.getExtras().get("data");
+            this.getContentResolver().notifyChange(UriFrenteDNI, null);
+            ContentResolver cr = this.getContentResolver();
+            try
+            {
+                bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, UriFrenteDNI);
+                //bitmap = scaleBitmap(bitmap);
+                Log.e("Bitmap", String.valueOf(bitmap));
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+                byte[] image=stream.toByteArray();
+
+                imgFrente.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 120, 120, false));
+                //mimageView.setImageBitmap(pmphoto);
+                sFotoTexto = android.util.Base64.encodeToString(image, 0);
+                //mimageView.setImageBitmap(bitmap);
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                Log.e("UriUri", "Failed to load", e);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        if (requestCode == CAMERA_DORSODNI && resultCode == RESULT_OK) {
+            //Bitmap pmphoto = (Bitmap) data.getExtras().get("data");
+            this.getContentResolver().notifyChange(UriFrenteDNI, null);
+            ContentResolver cr = this.getContentResolver();
+            try
+            {
+                bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, UriDorsoDNI);
+                //bitmap = scaleBitmap(bitmap);
+                Log.e("Bitmap", String.valueOf(bitmap));
+                ByteArrayOutputStream stream=new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 70, stream);
+                byte[] image=stream.toByteArray();
+
+                imgFrente.setImageBitmap(Bitmap.createScaledBitmap(bitmap, 120, 120, false));
+                //mimageView.setImageBitmap(pmphoto);
+                sFotoTexto = android.util.Base64.encodeToString(image, 0);
+                //mimageView.setImageBitmap(bitmap);
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT).show();
+                Log.e("UriUri", "Failed to load", e);
+            }
+
+        }
     }
 
 
